c8b010515b2a4b5641c77beefb50bd21579d1c41
HaroonAzhar <haroon.azhar@selecttechnologies.pk>
2023-10-02 22:12:30 +0500

    Order Listing With Pagination

---
+++ TTE Portal/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TTE_Portal.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Orders() { 
+        
+        return View();
+        
+        }
+
+        public IActionResult OrderDetails()
+        {
+            return View();
+        }
+    }
+}
