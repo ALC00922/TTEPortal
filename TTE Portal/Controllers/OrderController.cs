632e867cf908ec23a475e4828ef8a2c7b822ae86
Haroon Azhar <haroon.azhar@selecttechnologies.pk>
2023-10-02 23:44:02 +0500

    jquery issue fixed. jquery datatables working now. getting data from order table.

--- TTE Portal/Controllers/OrderController.cs
+++ TTE Portal/Controllers/OrderController.cs
@@ -1,18 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using TTE_Portal.Data;
+using TTE_Portal.ViewModels;
 
 namespace TTE_Portal.Controllers
 {
     public class OrderController : Controller
     {
+        private TTE_PortalContext _context;
+        string _userId = string.Empty;
+        public OrderController(TTE_PortalContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        [Authorize]
+        public IActionResult Orders()
+        {
+            List<OrdersViewModel> orders = new List<OrdersViewModel>();
+            // Inside a controller action or service method
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
 
-        public IActionResult Orders() { 
-        
-        return View();
-        
+            if (userIdClaim != null)
+            {
+                _userId = userIdClaim.Value;
+                // Now you have the user's ID in the 'userId' variable.
+            }
+            else
+            {
+                // User is not authenticated, or the claim doesn't exist.
+            }
+            var awbEntries = _context.AwbEntries.Where(x => x.CreatedBy == _userId).ToList();
+            var piEntries = _context.PIEntries.Where(x => x.CreatedBy == _userId).ToList();
+
+            for (int i = 0; i < awbEntries.Count; i++)
+            {
+                OrdersViewModel order = new OrdersViewModel();
+                order.PINO = awbEntries[i].PINo;
+                order.PIDate = piEntries[i].CreatedDate.ToShortDateString();
+                orders.Add(order);
+            }
+
+            return View(orders);
+
         }
 
         public IActionResult OrderDetails()
---
+++ TTE Portal/ViewModels/OrdersViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TTE_Portal.ViewModels
+{
+    public class OrdersViewModel
+    {
+        public string PINO { get; set; }
+        public string PIDate { get; set; }
+        public string Model { get; set; }
+        public string Status { get; set; }
+        public double   QTY { get; set; }
+        public string ImportedPINO { get; set; }
+    }
+}
