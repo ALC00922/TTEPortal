3b888823364a84749fca071c1270b675eaeeb99a
Haroon Azhar <haroon.azhar@selecttechnologies.pk>
2023-10-04 23:02:41 +0500

    Order Information Detail Model Binding to UI core functionality implemented. Downloading AWB File also implemented.

--- TTE Portal/Controllers/OrderController.cs
+++ TTE Portal/Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 using TTE_Portal.Data;
 using TTE_Portal.ViewModels;
+using static NuGet.Packaging.PackagingConstants;
 
 namespace TTE_Portal.Controllers
 {
@@ -20,7 +21,6 @@ namespace TTE_Portal.Controllers
         {
             return View();
         }
-        [Authorize]
         public IActionResult Orders()
         {
             List<OrdersViewModel> orders = new List<OrdersViewModel>();
@@ -36,8 +36,8 @@ namespace TTE_Portal.Controllers
             {
                 // User is not authenticated, or the claim doesn't exist.
             }
-            var awbEntries = _context.AwbEntries.Where(x => x.CreatedBy == _userId).ToList();
-            var piEntries = _context.PIEntries.Where(x => x.CreatedBy == _userId).ToList();
+            var awbEntries = _context.AwbEntries.Where(x => x.CreatedBy == "ef351a27-3983-408e-abda-4c61c9a3840a").ToList();
+            var piEntries = _context.PIEntries.Where(x => x.CreatedBy == "ef351a27-3983-408e-abda-4c61c9a3840a").ToList();
 
             for (int i = 0; i < awbEntries.Count; i++)
             {
@@ -45,7 +45,9 @@ namespace TTE_Portal.Controllers
                 order.PINO = awbEntries[i].PINo;
                 order.PIDate = piEntries[i].CreatedDate.ToShortDateString();
                 order.Model = piEntries[i].ItemDesc;
-                order.QTY =Convert.ToDouble(piEntries[i].ItemQty);
+                order.QTY = piEntries?[i]?.ItemQty !=null ?  Convert.ToDouble(piEntries[i].ItemQty).ToString() : "0";
+                order.BpCode = piEntries[i].BpCode;
+                order.UserId = "ef351a27-3983-408e-abda-4c61c9a3840a";
                 orders.Add(order);
             }
 
@@ -53,9 +55,33 @@ namespace TTE_Portal.Controllers
 
         }
 
-        public IActionResult OrderDetails()
+        public IActionResult OrderDetails(string bpCode, string userId)
         {
-            return View();
+            var awbEntries = _context.AwbEntries.Where(x => x.CreatedBy == userId && x.BpCode == bpCode).FirstOrDefault();
+            var piEntries = _context.PIEntries.Where(x => x.CreatedBy == userId && x.BpCode == bpCode).FirstOrDefault();
+            var delivery = _context.Deliveries.Where(x => x.CreatedBy == userId && x.BpCode == bpCode).FirstOrDefault();
+            var payment = _context.Payments.Where(x => x.CreatedBy == userId && x.BpCode == bpCode).FirstOrDefault();
+
+            OrdersViewModel order = new OrdersViewModel();
+            order.PINO = awbEntries.PINo;
+            order.PIDate = piEntries?.CreatedDate != null ? piEntries?.CreatedDate.ToString("dd-MM-yyyy") : DateTime.MinValue.ToString();
+            order.Model = piEntries.ItemBrand;
+            order.QTY = piEntries?.ItemQty != null ? Convert.ToDouble(piEntries.ItemQty).ToString() : "0";
+            order.AWB = piEntries?.CreatedDate != null ? piEntries?.CreatedDate.ToString("dd-MM-yyyy") : DateTime.MinValue.ToString();
+            order.Delivery = delivery?.CreatedDate != null ? delivery.CreatedDate.ToString("dd-MM-yyyy") : DateTime.MinValue.ToString();
+            order.UnitPrice = piEntries.ItemPrice != null ? Convert.ToDouble(piEntries.ItemPrice).ToString() : "0";
+            order.PIValue = piEntries?.PIValue != null ? Convert.ToDouble(piEntries.PIValue).ToString() : "0";
+            order.PaymentDate = payment?.PaymentDate != null ? payment.PaymentDate.ToString("dd-MM-yyyy") : DateTime.MinValue.ToString();
+            order.PaymentAmount = payment.Payments?.ToString();
+
+            order.DeliveryDCNo = delivery.ID.ToString();
+            order.DeliveryDate = delivery?.CreatedDate != null ? delivery.CreatedDate.ToString("dd-MM-yyyy") : DateTime.MinValue.ToString();
+            order.DeliveryModel = delivery.ItemBrand;
+            order.DeliveryQty = delivery.ItemQty.ToString();
+            order.DeliveryColor = delivery.ItemColor;
+
+            order.AWBFile = awbEntries.AwbFile;
+            return View(order);
         }
     }
 }
--- TTE Portal/ViewModels/OrdersViewModel.cs
+++ TTE Portal/ViewModels/OrdersViewModel.cs
@@ -6,7 +6,22 @@
         public string PIDate { get; set; }
         public string Model { get; set; }
         public string Status { get; set; }
-        public double   QTY { get; set; }
+        public string   QTY { get; set; }
         public string ImportedPINO { get; set; }
+        public string BpCode { get; set; }
+        public string UserId { get; set; }
+        public string AWB { get; set; }
+        public string Delivery { get; set; }
+        public string UnitPrice { get; set; }
+        public string PIValue { get; set; }
+        public string PaymentDate { get; set; }
+        public string PaymentAmount { get; set; }
+        public string PaymentRTGSNo { get; set; }
+        public string DeliveryDCNo { get; set; }
+        public string DeliveryDate { get; set; }
+        public string DeliveryModel { get; set; }
+        public string DeliveryQty { get; set; }
+        public string DeliveryColor { get; set; }
+        public string AWBFile { get; set; }
     }
 }
